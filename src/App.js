349ce59e43c2e532549c7323b34eddc3a4c212ec
sergio cordova <sergio.moi.cj@gmail.com>
2020-12-14 21:00:28 -0600

    first

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         My First React App
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <div>Sergio Cordova Jimenez</div>
       </header>
     </div>
   );
